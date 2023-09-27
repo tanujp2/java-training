ac20bf33faa13fe5a48c80aa137f93f733d27633
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-27 17:31:41 +0530

    updated file: formatted the output string XXh YYm ZZs

--- 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/SecondMinutesChallenge.java
+++ 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/SecondMinutesChallenge.java
@@ -4,6 +4,7 @@ public class SecondMinutesChallenge {
 	public static void main(String[] args) {
 		System.out.println(getDurationString(3945)); // this is the first test case
 		System.out.println(getDurationString(3601, 48)); // this is the second test
+		System.out.println(getDurationString(65, 45)); // this is the third test
 	}
 
 	public static String getDurationString(int seconds) {
@@ -25,6 +26,6 @@ public class SecondMinutesChallenge {
 		int hours = minutes / 60;
 		minutes %= 60;
 
-		return hours + "h " + minutes + "m " + seconds + "s";
+		return String.format("%02dh %02dm %02ds", hours, minutes, seconds);
 	}
 }
