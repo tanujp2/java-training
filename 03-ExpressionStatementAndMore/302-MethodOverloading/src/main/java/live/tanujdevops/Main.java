b003420619aacf8c4513f0063ecb97631fd90be6
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-27 16:32:45 +0530

    updated file: has errors

--- 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/Main.java
+++ 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		System.out.println("New score is " + newScore);
 
 		calculateScore(75);
+		
+		calculateScore();
 	}
 
 	public static int calculateScore(String playerName, int score) {
