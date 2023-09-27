ba2af38613c143e0e3edcc1c8269a84d8f7cc124
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-27 17:38:27 +0530

    updated file: added validation messages

--- 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/SecondMinutesChallenge.java
+++ 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/SecondMinutesChallenge.java
@@ -2,14 +2,17 @@ package live.tanujdevops;
 
 public class SecondMinutesChallenge {
 	public static void main(String[] args) {
-		System.out.println(getDurationString(3945)); // this is the first test case
-		System.out.println(getDurationString(3601, 48)); // this is the second test
-		System.out.println(getDurationString(65, 45)); // this is the third test
+		System.out.println(getDurationString(3945));
+		System.out.println(getDurationString(3601, 48));
+		System.out.println(getDurationString(65, 45));
+		System.out.println(getDurationString(-45));
+		System.out.println(getDurationString(-45, 40));
+		System.out.println(getDurationString(75, 60));
 	}
 
 	public static String getDurationString(int seconds) {
 		if (seconds < 0) {
-			return "Invalid seconds";
+			return "Invalid seconds. Required: positive seconds, Actual: " + seconds;
 		}
 
 		int minutes = seconds / 60;
@@ -20,7 +23,8 @@ public class SecondMinutesChallenge {
 
 	public static String getDurationString(int minutes, int seconds) {
 		if (minutes < 0 || seconds < 0 || seconds > 59) {
-			return "Invalid minutes and seconds";
+			return "Invalid minutes and seconds. Required: positive minutes and seconds (0-59), Actual: minutes = "
+					+ minutes + " and seconds = " + seconds;
 		}
 
 		int hours = minutes / 60;
