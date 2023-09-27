fde9c3190e0319e57bb214f94337b4f1e60ff772
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-27 16:30:18 +0530

    updated file

--- 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/Main.java
+++ 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		int newScore = calculateScore("Tanuj", 500);
 		System.out.println("New score is " + newScore);
+
+		calculateScore(75);
 	}
 
 	public static int calculateScore(String playerName, int score) {
