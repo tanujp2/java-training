dda419f21f6c185cbaf3d677ba879460717da015
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-27 06:08:39 +0530

    updated exercise 5

--- 03-ExpressionStatementAndMore/CodingExercises/src/main/java/live/tanujdevops/BarkingDog.java
+++ 03-ExpressionStatementAndMore/CodingExercises/src/main/java/live/tanujdevops/BarkingDog.java
@@ -11,10 +11,10 @@ public class BarkingDog {
 	public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
 		if (hourOfDay < 0 || hourOfDay > 23) {
 			return false;
-		}
-
-		if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
-			return true;
+		} else if (hourOfDay < 8 || hourOfDay > 22) {
+			if (barking) {
+				return true;
+			}
 		}
 
 		return false;
