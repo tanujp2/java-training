52447e42da36debf9c3435ca73cae952941b546a
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-27 17:26:23 +0530

    added minutes and seconds challenge demostrating method overloading

---
+++ 03-ExpressionStatementAndMore/302-MethodOverloading/src/main/java/live/tanujdevops/SecondMinutesChallenge.java
@@ -0,0 +1,30 @@
+package live.tanujdevops;
+
+public class SecondMinutesChallenge {
+	public static void main(String[] args) {
+		System.out.println(getDurationString(3945)); // this is the first test case
+		System.out.println(getDurationString(3601, 48)); // this is the second test
+	}
+
+	public static String getDurationString(int seconds) {
+		if (seconds < 0) {
+			return "Invalid seconds";
+		}
+
+		int minutes = seconds / 60;
+		seconds %= 60;
+
+		return getDurationString(minutes, seconds);
+	}
+
+	public static String getDurationString(int minutes, int seconds) {
+		if (minutes < 0 || seconds < 0 || seconds > 59) {
+			return "Invalid minutes and seconds";
+		}
+
+		int hours = minutes / 60;
+		minutes %= 60;
+
+		return hours + "h " + minutes + "m " + seconds + "s";
+	}
+}
