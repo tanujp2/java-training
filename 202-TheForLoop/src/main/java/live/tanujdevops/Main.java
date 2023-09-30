90b4cffbd2ec74a3d13b5e3d50c7c05767702c5b
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-09-30 23:11:51 +0530

    added file: using for loop

---
+++ 202-TheForLoop/src/main/java/live/tanujdevops/Main.java
@@ -0,0 +1,9 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		for (int counter = 1; counter <= 5; counter++) {
+			System.out.println(counter);
+		}
+	}
+}
