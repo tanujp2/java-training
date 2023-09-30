a777c5a331a624d5983c2f7edba53bf36781533c
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-09-30 23:26:13 +0530

    updated file: mini challenge

--- 202-TheForLoop/src/main/java/live/tanujdevops/Main.java
+++ 202-TheForLoop/src/main/java/live/tanujdevops/Main.java
@@ -12,6 +12,9 @@ public class Main {
 
 		for (double rate = 7.5; rate <= 10; rate += 0.25) {
 			double interest = calculateInterest(100, rate);
+			if (interest > 8.5) {
+				break;
+			}
 			System.out.println("100 at " + rate + "% interest = " + interest);
 		}
 
