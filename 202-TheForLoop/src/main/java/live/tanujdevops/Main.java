8653dc9f8a5a856faba9022010b4c09f9443334b
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-09-30 23:15:04 +0530

    updated file

--- 202-TheForLoop/src/main/java/live/tanujdevops/Main.java
+++ 202-TheForLoop/src/main/java/live/tanujdevops/Main.java
@@ -5,5 +5,14 @@ public class Main {
 		for (int counter = 1; counter <= 5; counter++) {
 			System.out.println(counter);
 		}
+
+		System.out.println("10,000 at 2% interest = " + calculateInterest(10000, 2));
+		System.out.println("10,000 at 3% interest = " + calculateInterest(10000, 3));
+		System.out.println("10,000 at 4% interest = " + calculateInterest(10000, 4));
+		System.out.println("10,000 at 5% interest = " + calculateInterest(10000, 5));
+	}
+
+	public static double calculateInterest(double amount, double interestRate) {
+		return amount * (interestRate / 100);
 	}
 }
