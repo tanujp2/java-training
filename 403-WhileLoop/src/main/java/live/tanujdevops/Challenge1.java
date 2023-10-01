62d566996d206681840636daea83dbe09c64e140
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-01 23:19:54 +0530

    updated file: using while loop

--- 403-WhileLoop/src/main/java/live/tanujdevops/Challenge1.java
+++ 403-WhileLoop/src/main/java/live/tanujdevops/Challenge1.java
@@ -2,10 +2,12 @@ package live.tanujdevops;
 
 public class Challenge1 {
 	public static void main(String[] args) {
-		for (int number = 5; number <= 20; number++) {
+		int number = 5;
+		while (number <= 20) {
 			if (isEvenNumber(number)) {
 				System.out.println(number + " is even");
 			}
+			number++;
 		}
 	}
 
