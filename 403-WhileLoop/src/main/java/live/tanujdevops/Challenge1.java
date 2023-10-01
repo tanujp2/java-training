23ecd24875e9dd4445d07fbf72447aa842709f57
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-01 23:27:50 +0530

    updated file: total even and odd numbers

--- 403-WhileLoop/src/main/java/live/tanujdevops/Challenge1.java
+++ 403-WhileLoop/src/main/java/live/tanujdevops/Challenge1.java
@@ -3,12 +3,24 @@ package live.tanujdevops;
 public class Challenge1 {
 	public static void main(String[] args) {
 		int number = 5;
+		int totalEven = 0;
+		int totalOdd = 0;
+
 		while (number <= 20) {
 			if (isEvenNumber(number)) {
+				totalEven++;
 				System.out.println(number + " is even");
+				if (totalEven == 5) {
+					break;
+				}
+			} else {
+				totalOdd++;
 			}
 			number++;
 		}
+
+		System.out.println("Total even = " + totalEven);
+		System.out.println("Total odd = " + totalOdd);
 	}
 
 	public static boolean isEvenNumber(int number) {
