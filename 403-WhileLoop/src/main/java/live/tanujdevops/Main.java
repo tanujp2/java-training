5b21ef60676242ffbb5a8e910574dae77f77e461
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-01 21:15:34 +0530

    added file: difference b/w for loop and while loop

---
+++ 403-WhileLoop/src/main/java/live/tanujdevops/Main.java
@@ -0,0 +1,15 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+
+		int j = 1;
+		while (j <= 5) {
+			System.out.println(j);
+			j++;
+		}
+	}
+}
