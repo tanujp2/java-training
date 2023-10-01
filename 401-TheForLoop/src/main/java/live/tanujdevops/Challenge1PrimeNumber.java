a2a23014d52b56e62d94f8154e626db24fa2f5e2
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-01 20:38:43 +0530

    reorganized files

---
+++ 401-TheForLoop/src/main/java/live/tanujdevops/Challenge1PrimeNumber.java
@@ -0,0 +1,26 @@
+package live.tanujdevops;
+
+public class Challenge1PrimeNumber {
+	public static void main(String[] args) {
+		System.out.println("0 is" + (isPrime(0) ? " " : " NOT ") + "a prime number");
+		System.out.println("1 is" + (isPrime(1) ? " " : " NOT ") + "a prime number");
+		System.out.println("2 is" + (isPrime(2) ? " " : " NOT ") + "a prime number");
+		System.out.println("3 is" + (isPrime(3) ? " " : " NOT ") + "a prime number");
+		System.out.println("8 is" + (isPrime(8) ? " " : " NOT ") + "a prime number");
+		System.out.println("17 is" + (isPrime(17) ? " " : " NOT ") + "a prime number");
+	}
+
+	public static boolean isPrime(int number) {
+		if (number < 2) {
+			return false;
+		}
+
+		for (int i = 2; i <= number / 2; i++) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
