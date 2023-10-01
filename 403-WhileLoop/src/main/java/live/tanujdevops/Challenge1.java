0fe40bbb4ccfb17c9f2782679655ebe00f3401ed
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-01 23:31:16 +0530

    updated file

--- 403-WhileLoop/src/main/java/live/tanujdevops/Challenge1.java
+++ 403-WhileLoop/src/main/java/live/tanujdevops/Challenge1.java
@@ -3,24 +3,24 @@ package live.tanujdevops;
 public class Challenge1 {
 	public static void main(String[] args) {
 		int number = 5;
-		int totalEven = 0;
-		int totalOdd = 0;
+		int countEven = 0;
+		int countOdd = 0;
 
 		while (number <= 20) {
 			if (isEvenNumber(number)) {
-				totalEven++;
+				countEven++;
 				System.out.println(number + " is even");
-				if (totalEven == 5) {
+				if (countEven == 5) {
 					break;
 				}
 			} else {
-				totalOdd++;
+				countOdd++;
 			}
 			number++;
 		}
 
-		System.out.println("Total even = " + totalEven);
-		System.out.println("Total odd = " + totalOdd);
+		System.out.println("Total even = " + countEven);
+		System.out.println("Total odd = " + countOdd);
 	}
 
 	public static boolean isEvenNumber(int number) {
