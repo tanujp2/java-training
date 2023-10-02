5b5d1b9fd8a55d6f6a17b93d1572f5188db0cdc8
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-02 09:00:34 +0530

    added exercise 21: all factors

---
+++ CodingExercises/src/main/java/live/tanujdevops/FactorPrinter.java
@@ -0,0 +1,25 @@
+package live.tanujdevops;
+
+public class FactorPrinter {
+	public static void main(String[] args) {
+		printFactors(6);
+		printFactors(32);
+		printFactors(10);
+		printFactors(-1);
+	}
+
+	public static void printFactors(int number) {
+		if (number < 1) {
+			System.out.println("Invalid Value");
+			return;
+		}
+
+		for (int i = 1; i <= number / 2; i++) {
+			if (number % i == 0) {
+				System.out.print(i + " ");
+			}
+		}
+
+		System.out.println(number);
+	}
+}
