3197a83c3a87a8cd8be465483486dd868792981c
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-02 22:03:11 +0530

    added file: mathematical operation on strings

---
+++ 404-ReadingUserInput/src/main/java/live/tanujdevops/Main.java
@@ -0,0 +1,9 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		String currentYear = "2023";
+		String usersDateOfBirth = "1999";
+		System.out.println("Age = " + (currentYear + usersDateOfBirth));
+	}
+}
