876864ea6deca65a216fd51624e02abc030bbdd0
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-02 22:18:33 +0530

    updated file: using system console to read input, ide disable the access to console, hence use regular console

--- 404-ReadingUserInput/src/main/java/live/tanujdevops/Main.java
+++ 404-ReadingUserInput/src/main/java/live/tanujdevops/Main.java
@@ -3,15 +3,29 @@ package live.tanujdevops;
 public class Main {
 	public static void main(String[] args) {
 		int currentYear = 2023;
-		String usersDateOfBirth = "1999";
+//		String usersDateOfBirth = "1999";
+//
+//		int dateOfBirth = Integer.parseInt(usersDateOfBirth);
+//
+//		System.out.println("Age = " + (currentYear - dateOfBirth));
+//
+//		String usersAgeWithPartialYear = "22.5";
+//		double ageWithPartialYear = Double.parseDouble(usersAgeWithPartialYear);
+//
+//		System.out.println("user say he's " + ageWithPartialYear);
 
-		int dateOfBirth = Integer.parseInt(usersDateOfBirth);
+		System.out.println(getInputFromConsole(currentYear));
+		System.out.println(getInputFromScanner(currentYear));
 
-		System.out.println("Age = " + (currentYear - dateOfBirth));
+	}
 
-		String usersAgeWithPartialYear = "22.5";
-		double ageWithPartialYear = Double.parseDouble(usersAgeWithPartialYear);
+	public static String getInputFromConsole(int currentYear) {
+		String name = System.console().readLine("Hi, What's your name? ");
+		System.out.println("Hi " + name + ", Thanks for tanking the course!");
+		return "";
+	}
 
-		System.out.println("user say he's " + ageWithPartialYear);
+	public static String getInputFromScanner(int currentYear) {
+		return "";
 	}
 }
