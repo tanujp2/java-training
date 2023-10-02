4bc8985836463c236b8e6b67ed093ec0f00702a6
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-02 22:08:36 +0530

    updated file: parsing string to primitive types

--- 404-ReadingUserInput/src/main/java/live/tanujdevops/Main.java
+++ 404-ReadingUserInput/src/main/java/live/tanujdevops/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		int currentYear = 2023;
 		String usersDateOfBirth = "1999";
-		System.out.println("Age = " + (currentYear + usersDateOfBirth));
+
+		int dateOfBirth = Integer.parseInt(usersDateOfBirth);
+
+		System.out.println("Age = " + (currentYear - dateOfBirth));
 	}
 }
