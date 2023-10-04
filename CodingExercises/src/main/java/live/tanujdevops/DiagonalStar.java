84d38632e799457a0ab3cd8df3c3a7a23be8f120
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-04 21:27:59 +0530

    added exercise 27: diagonal star, incomplete

---
+++ CodingExercises/src/main/java/live/tanujdevops/DiagonalStar.java
@@ -0,0 +1,12 @@
+package live.tanujdevops;
+
+public class DiagonalStar {
+
+	public static void printSquareStar(int number) {
+		if (number < 5) {
+			System.out.println("Invalid Value");
+			return;
+		}
+	}
+
+}
