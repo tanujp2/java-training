23173211a7441b517dda31c9da1a646023b58e98
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-04 22:07:57 +0530

    updated files: updating private field of a class outside using public setter methods

--- 501-classes/src/main/java/live/tanujdevops/Car.java
+++ 501-classes/src/main/java/live/tanujdevops/Car.java
@@ -27,6 +27,26 @@ public class Car {
 		return convertible;
 	}
 
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+
+	public void setConvertible(boolean convertible) {
+		this.convertible = convertible;
+	}
+
 	public void describeCar() {
 		System.out.println(
 				doors + "-Door " + color + " " + make + " " + model + " " + (convertible ? "Convertible" : ""));
--- 501-classes/src/main/java/live/tanujdevops/Main.java
+++ 501-classes/src/main/java/live/tanujdevops/Main.java
@@ -3,9 +3,13 @@ package live.tanujdevops;
 public class Main {
 	public static void main(String[] args) {
 		Car car = new Car();
-//		car.make = "Tata";
-//		car.model = "Nexon XMS";
-//		car.color = "White";
+
+		car.setMake("Tata");
+		car.setModel("Nexon XMS");
+		car.setColor("White");
+		car.setConvertible(false);
+		car.setDoors(4);
+
 		System.out.println("Make = " + car.getMake());
 		System.out.println("Model = " + car.getModel());
 		car.describeCar();
