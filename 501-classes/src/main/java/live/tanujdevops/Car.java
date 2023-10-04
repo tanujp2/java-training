145f264a1c5b87e09a053f07e9f245c24a09f798
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-04 22:00:28 +0530

    updated files: accessing private field of a class outside using public getters method

--- 501-classes/src/main/java/live/tanujdevops/Car.java
+++ 501-classes/src/main/java/live/tanujdevops/Car.java
@@ -7,6 +7,26 @@ public class Car {
 	private int doors = 2;
 	private boolean convertible = true;
 
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getDoors() {
+		return doors;
+	}
+
+	public boolean isConvertible() {
+		return convertible;
+	}
+
 	public void describeCar() {
 		System.out.println(
 				doors + "-Door " + color + " " + make + " " + model + " " + (convertible ? "Convertible" : ""));
--- 501-classes/src/main/java/live/tanujdevops/Main.java
+++ 501-classes/src/main/java/live/tanujdevops/Main.java
@@ -6,6 +6,8 @@ public class Main {
 //		car.make = "Tata";
 //		car.model = "Nexon XMS";
 //		car.color = "White";
+		System.out.println("Make = " + car.getMake());
+		System.out.println("Model = " + car.getModel());
 		car.describeCar();
 	}
 }
