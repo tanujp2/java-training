8a8e08ab633049a181e94ea66fba6345b020541d
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-04 21:55:29 +0530

    updated files: private fields cannot be accessed directly in any other class

--- 501-classes/src/main/java/live/tanujdevops/Car.java
+++ 501-classes/src/main/java/live/tanujdevops/Car.java
@@ -1,11 +1,11 @@
 package live.tanujdevops;
 
 public class Car {
-	private String make;
-	private String model;
-	private String color;
-	private int doors;
-	private boolean convertible;
+	private String make = "Tesla";
+	private String model = "Model X";
+	private String color = "Gray";
+	private int doors = 2;
+	private boolean convertible = true;
 
 	public void describeCar() {
 		System.out.println(
--- 501-classes/src/main/java/live/tanujdevops/Main.java
+++ 501-classes/src/main/java/live/tanujdevops/Main.java
@@ -3,6 +3,9 @@ package live.tanujdevops;
 public class Main {
 	public static void main(String[] args) {
 		Car car = new Car();
+//		car.make = "Tata";
+//		car.model = "Nexon XMS";
+//		car.color = "White";
 		car.describeCar();
 	}
 }
