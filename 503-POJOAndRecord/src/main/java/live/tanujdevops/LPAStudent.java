c39c97028f316e8220b57b22c23656b35ec35a55
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-09 23:10:05 +0530

    added a record pojo

---
+++ 503-POJOAndRecord/src/main/java/live/tanujdevops/LPAStudent.java
@@ -0,0 +1,5 @@
+package live.tanujdevops;
+
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
+
+}
--- 503-POJOAndRecord/src/main/java/live/tanujdevops/Main.java
+++ 503-POJOAndRecord/src/main/java/live/tanujdevops/Main.java
@@ -3,7 +3,7 @@ package live.tanujdevops;
 public class Main {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
-			Student s = new Student("S92300" + i, switch (i) {
+			LPAStudent s = new LPAStudent("S92300" + i, switch (i) {
 			case 1 -> "Mary";
 			case 2 -> "Carol";
 			case 3 -> "Time";
@@ -14,5 +14,14 @@ public class Main {
 
 			System.out.println(s);
 		}
+
+		Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java Masterclass");
+		LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05/11/1985", "Java Masterclass");
+
+		System.out.println(pojoStudent);
+		System.out.println(recordStudent);
+
+		System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+		System.out.println(recordStudent.getName() + " is taking " + recordStudent.getClassList());
 	}
 }
