f1bef779caf8a1a0dad443e44ca951127139c9ec
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-09 20:57:09 +0530

    added constructor challenge

--- 502-Constructors/src/main/java/live/tanujdevops/Main.java
+++ 502-Constructors/src/main/java/live/tanujdevops/Main.java
@@ -30,5 +30,18 @@ public class Main {
 
 		Account timsAccount = new Account("Tim", "tim@email.com", "12345");
 		System.out.println("AccountNo: " + timsAccount.getNumber() + "; name: " + timsAccount.getCustomerName());
+
+//		customer test
+		Customer tanuj = new Customer();
+		System.out.println("Customer name: " + tanuj.getName() + "; credit limit: " + tanuj.getCreditLimit()
+				+ "; email: " + tanuj.getEmail());
+
+		Customer praveen = new Customer("Praveen", 5000.0, "praveen@email.com");
+		System.out.println("Customer name: " + praveen.getName() + "; credit limit: " + praveen.getCreditLimit()
+				+ "; email: " + praveen.getEmail());
+
+		Customer anand = new Customer("Anand", "anand@email.com");
+		System.out.println("Customer name: " + anand.getName() + "; credit limit: " + anand.getCreditLimit()
+				+ "; email: " + anand.getEmail());
 	}
 }
