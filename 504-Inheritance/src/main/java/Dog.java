9d050ff68cd97c82f17eb2b84cf8e53fd3cbacbe
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 00:13:07 +0530

    added specific methods

--- 504-Inheritance/src/main/java/Dog.java
+++ 504-Inheritance/src/main/java/Dog.java
@@ -29,7 +29,30 @@ public class Dog extends Animal {
 	@Override
 	public void move(String speed) {
 		super.move(speed);
-		System.out.println("Dogs walk, run and wag their tail");
+//		System.out.println("Dogs walk, run and wag their tail");
+		if (speed == "slow") {
+			walk();
+			wagTail();
+		} else {
+			run();
+			bark();
+		}
+		System.out.println();
 	}
 
+	private void bark() {
+		System.out.println("Woof! ");
+	}
+
+	private void run() {
+		System.out.println("Dog running ");
+	}
+
+	private void walk() {
+		System.out.println("Dog walking ");
+	}
+
+	private void wagTail() {
+		System.out.println("Tail wagging ");
+	}
 }
