33cbee193c27a6c6fb3499845d0a15f0f83c3f3f
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 01:20:25 +0530

    added files

--- 506-InheritanceChallenge/src/main/java/Main.java
+++ 506-InheritanceChallenge/src/main/java/Main.java
@@ -5,5 +5,10 @@ public class Main {
 		System.out.println(tim);
 		System.out.println("Age = " + tim.getAge());
 		System.out.println("Pay = " + tim.collectPay());
+
+		Employee joe = new Employee("Joe", "11/11/1990", "03/03/2020");
+		System.out.println(joe);
+		System.out.println("Age = " + joe.getAge());
+		System.out.println("Pay = " + joe.collectPay());
 	}
 }
