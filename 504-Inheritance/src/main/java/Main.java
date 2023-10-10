c5fba760e878ae9362fa915ceddb5c83669a7ed8
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 00:22:57 +0530

    updated files

--- 504-Inheritance/src/main/java/Main.java
+++ 504-Inheritance/src/main/java/Main.java
@@ -12,6 +12,9 @@ public class Main {
 
 		Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
 		doAnimalStuff(retriever, "slow");
+		
+		Dog wolf = new Dog("Wolf", 40);
+		doAnimalStuff(wolf, "slow");
 
 	}
 
