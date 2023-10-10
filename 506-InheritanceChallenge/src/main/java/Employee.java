9c27e8c7cb960987c882586a4d48e102bee94cfc
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 01:19:14 +0530

    added files

--- 506-InheritanceChallenge/src/main/java/Employee.java
+++ 506-InheritanceChallenge/src/main/java/Employee.java
@@ -2,13 +2,14 @@
 public class Employee extends Worker {
 	private long employeeId;
 	private String hireDate;
+	private static int employeeNo = 1;
 
 	public Employee() {
 	}
 
-	public Employee(String name, String birthDate, long employeeId, String hireDate) {
+	public Employee(String name, String birthDate, String hireDate) {
 		super(name, birthDate);
-		this.employeeId = employeeId;
+		this.employeeId = Employee.employeeNo++;
 		this.hireDate = hireDate;
 	}
 
--- 506-InheritanceChallenge/src/main/java/Main.java
+++ 506-InheritanceChallenge/src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Employee tim = new Employee("Tim", "11/11/1985", 77001, "01/01/2020");
+		Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
 		System.out.println(tim);
 		System.out.println("Age = " + tim.getAge());
 		System.out.println("Pay = " + tim.collectPay());
