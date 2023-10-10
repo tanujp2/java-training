d77221b3dde03793507e171527fbea71963f7d5e
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-10 22:18:38 +0530

    added exercise 34: carpet cost calculator

---
+++ CodingExercises/src/main/java/live/tanujdevops/Carpet.java
@@ -0,0 +1,18 @@
+package live.tanujdevops;
+
+public class Carpet {
+	private double cost;
+
+	public Carpet(double cost) {
+		this.cost = cost;
+
+		if (cost < 0) {
+			this.cost = 0;
+		}
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+}
