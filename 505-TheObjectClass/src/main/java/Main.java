fed7df8e22479fe55c256e297a1bf883954ef32b
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 00:55:53 +0530

    updated files: overriden toString() method

--- 505-TheObjectClass/src/main/java/Main.java
+++ 505-TheObjectClass/src/main/java/Main.java
@@ -3,6 +3,9 @@ public class Main extends Object {
 	public static void main(String[] args) {
 		Student max = new Student("Max", 21);
 		System.out.println(max);
+
+		PrimarySchoolStudent jimmy = new PrimarySchoolStudent("Jimmy", 8, "Carole");
+		System.out.println(jimmy);
 	}
 }
 
@@ -34,5 +37,11 @@ class PrimarySchoolStudent extends Student {
 	PrimarySchoolStudent(String name, int age, String parentName) {
 		super(name, age);
 		this.parentName = parentName;
+	}
+
+	@Override
+	public String toString() {
+//		return "Student [name=" + name + ", age=" + age + "]";
+		return parentName + "'s kid,  " + super.toString();
 	}
 }
