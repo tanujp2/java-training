5226995322efd2182557dc7daf3054120533c454
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-10 22:26:55 +0530

    added exercise 35: complex operations

---
+++ CodingExercises/src/main/java/live/tanujdevops/ComplexNumber.java
@@ -0,0 +1,54 @@
+package live.tanujdevops;
+
+public class ComplexNumber {
+	private double real;
+	private double imaginary;
+
+	public ComplexNumber(double real, double imaginary) {
+		this.real = real;
+		this.imaginary = imaginary;
+	}
+
+	public double getReal() {
+		return real;
+	}
+
+	public double getImaginary() {
+		return imaginary;
+	}
+
+	public void add(double real, double imaginary) {
+		this.real += real;
+		this.imaginary += imaginary;
+	}
+
+	public void add(ComplexNumber cn) {
+		this.real += cn.real;
+		this.imaginary += cn.imaginary;
+	}
+
+	public void subtract(double real, double imaginary) {
+		this.real -= real;
+		this.imaginary -= imaginary;
+	}
+
+	public void subtract(ComplexNumber cn) {
+		this.real -= cn.real;
+		this.imaginary -= cn.imaginary;
+	}
+
+	public static void main(String[] args) {
+		ComplexNumber one = new ComplexNumber(1.0, 1.0);
+		ComplexNumber number = new ComplexNumber(2.5, -1.5);
+		one.add(1, 1);
+		System.out.println("one.real= " + one.getReal());
+		System.out.println("one.imaginary= " + one.getImaginary());
+		one.subtract(number);
+		System.out.println("one.real= " + one.getReal());
+		System.out.println("one.imaginary= " + one.getImaginary());
+		number.subtract(one);
+		System.out.println("number.real= " + number.getReal());
+		System.out.println("number.imaginary= " + number.getImaginary());
+	}
+
+}
