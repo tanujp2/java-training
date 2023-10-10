434baa43afd5c9d9f87f026bd78fc6d861cf4c62
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 01:12:41 +0530

    added files

--- 506-InheritanceChallenge/src/main/java/Worker.java
+++ 506-InheritanceChallenge/src/main/java/Worker.java
@@ -4,15 +4,32 @@ public class Worker {
 	private String birthDate;
 	private String endDate;
 
+	public Worker() {
+	}
+
+	public Worker(String name, String birthDate) {
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
 	public int getAge() {
-		return -1;
+		int currentYear = 2025;
+		int birthYear = Integer.parseInt(birthDate.substring(6)); // mm/dd/yyy
+
+		return currentYear - birthYear;
 	}
 
 	public double collectPay() {
-		return -1;
+		return 0.0;
 	}
 
 	public void terminate(String endDate) {
-
+		this.endDate = endDate;
 	}
+
+	@Override
+	public String toString() {
+		return "Worker [name=" + name + ", birthDate=" + birthDate + ", endDate=" + endDate + "]";
+	}
+
 }
