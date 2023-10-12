2817d4cb112d24a92eaf9a34baa39c9681c5936f
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-12 22:28:42 +0530

    updated files: normal string does not support multilining

---
+++ 507-TextBlock/src/main/java/live/tanujdevops/Main.java
@@ -0,0 +1,8 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		String bulletIt = "Print a bulleted list: " + "\u2022 First point" + "\u2022 Second poing";
+		System.out.println(bulletIt);
+	}
+}
