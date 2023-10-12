b4c678dda6f88777daf9016dfadf4f3a8f003bc1
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-12 22:38:06 +0530

    updated files

--- 507-TextBlock/src/main/java/live/tanujdevops/Main.java
+++ 507-TextBlock/src/main/java/live/tanujdevops/Main.java
@@ -2,7 +2,17 @@ package live.tanujdevops;
 
 public class Main {
 	public static void main(String[] args) {
-		String bulletIt = "Print a bulleted list: " + "\u2022 First point" + "\u2022 Second poing";
+		String bulletIt = "Print a bulleted list: " + "\n\t\u2022 First point" + "\n\t\u2022 Second point";
 		System.out.println(bulletIt);
+
+		String textBlock = """
+				Print a bulleted list
+					\u2022 First point
+						\u2022 Sub point
+				""";
+		System.out.println(textBlock);
+
+		int age = 35;
+		System.out.printf("Your age is %d", age);
 	}
 }
