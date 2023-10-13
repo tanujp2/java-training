5594073e92c901a511e71d8c51beca411c4e167b
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-13 06:38:57 +0530

    updated files: using simple print

---
+++ 508-FormattingString/src/main/java/live/tanujdevops/Main.java
@@ -0,0 +1,9 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		String name = "John";
+		int age = 20;
+		System.out.println(name + " is " + age);
+	}
+}
