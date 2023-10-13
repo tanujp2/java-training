3125f8ef6d4ff20ea44a7150ad0598916b89feb4
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-13 22:43:16 +0530

    updated file: extracting a part of string

--- 509-StringComparisonMethods/src/main/java/live/tanujdevops/Main.java
+++ 509-StringComparisonMethods/src/main/java/live/tanujdevops/Main.java
@@ -2,6 +2,9 @@ package live.tanujdevops;
 
 public class Main {
 	public static void main(String[] args) {
-
+		String birthDate = "25/11/1982";
+		int startingIndex = birthDate.indexOf("1982");
+		System.out.println("startingIndex = " + startingIndex);
+		System.out.println("Birth year = " + birthDate.substring(startingIndex));
 	}
 }
