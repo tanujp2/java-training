d8afe6ec50a2e2690f8270fdb1a1e70b29ddad97
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-14 05:43:22 +0530

    updated file: stringbuilder capacity

--- _510-StringBuilder/src/main/java/live/tanujdevops/App.java
+++ _510-StringBuilder/src/main/java/live/tanujdevops/App.java
@@ -12,6 +12,12 @@ public class App {
 
         printInformation(helloWorld);
         printInformation(helloWorldBuilder);
+
+        StringBuilder emptyStart = new StringBuilder(); //default capacity is 16, next capacity is determined by the JVM
+        StringBuilder emptyStart32 = new StringBuilder(32);
+
+        printInformation(emptyStart);
+        printInformation(emptyStart32);
     }
 
     public static void printInformation(String string) {
@@ -22,5 +28,6 @@ public class App {
     public static void printInformation(StringBuilder builder) {
         System.out.println("StringBuilder = " + builder);
         System.out.println("length = " + builder.length());
+        System.out.println("capacity = " + builder.capacity());
     }
 }
